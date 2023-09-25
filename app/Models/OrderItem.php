56082fedec8ae4475695844f06ce734588551cfe
Serius17 <92595056+Serius17@users.noreply.github.com>
2023-09-25 21:55:31 +0700

    set Up Relation Model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderItem extends Model
 {
+    protected $guarded = [];
     use HasFactory;
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Pengguna extends Model
 {
+    protected $guarded = [];
     use HasFactory;
+
+    public function orderItem(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
